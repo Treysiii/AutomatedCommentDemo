40d99ca44694818e94a19e68aa362bdade29bbb4
Treysiii <tracylykabolandos25@gmail.com>
2026-04-07 17:21:29 +0800

    ILAMATHS 2 Add text and style it

---
+++ HelloWorldBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello-world-box">
+    <span class="hello-world-text">Hello World</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorldBox',
+};
+</script>
+
+<style scoped>
+.hello-world-text {
+  font-size: 30px;
+  color: green;
+  font-weight: 400;
+}
+</style>
