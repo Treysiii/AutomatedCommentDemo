40580c36208aa058a2a3af62022cec479d421da5
Treysiii <tracylykabolandos25@gmail.com>
2026-04-13 14:02:28 +0800

    Revert "add dynamic messages and color props"
    This reverts commit d492d7deafa04dc7187a615dccb5502318ec843e.

--- HelloWorldBox.vue
+++ HelloWorldBox.vue
@@ -1,34 +1,25 @@
 <template>
   <div class="hello-world-box">
-    <span
-      v-for="(msg, i) in messages"
-      :key="i"
-      class="hello-world-text"
-      :style="{ color: color }"
-    >{{ msg }}</span>
+    <span class="hello-world-text">Hello World</span>
+    <span class="hello-world-text">Hello World</span>
   </div>
+
+  <div class="hello-world-box">
+    <span class="hello-world-text">Hello World</span>
+    <span class="hello-world-text">Hello World</span>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorldBox',
-  props: {
-    messages: {
-      type: Array,
-      default: () => ['Hello World', 'Hello World'],
-    },
-    color: {
-      type: String,
-      default: 'green',
-    },
-  },
 };
 </script>
 
 <style scoped>
 .hello-world-text {
   font-size: 30px;
+  color: green;
   font-weight: 400;
-  display: block;
 }
 </style>
